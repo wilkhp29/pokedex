9c16e0eafca1ef5d4923b61c2cb36258d6e08db2
william.wos <william.wos@policiacientifica.sp.gov.br>
2019-09-19 19:53:46 -0300

    fim .

--- src/store/index.js
+++ src/store/index.js
@@ -2,13 +2,13 @@ import { createStore , applyMiddleware } from "redux";
 import { persistStore , persistReducer } from  "redux-persist";
 import createSagaMiddleware from "redux-saga";
 import storage from "redux-persist/lib/storage";
-
-import { getToken } from "../services/auth"
 import rootReducer from "./reducers";
 import sagas from "./sagas";
 
+
+
 const persistConfig = {
-    key:getToken,
+    key:"pokedex",
     storage
 };
 
